379bf1f880b99b33b1a58de7488fb0286f9acf15
Glorieux Lukama <ir.antoineglorieux@gmail.com>
2023-12-08 14:06:10 +0200

    feat (prod): add analytics tag

--- src/root.tsx
+++ src/root.tsx
@@ -1,32 +1,39 @@
 import { component$ } from "@builder.io/qwik";
 import {
-  QwikCityProvider,
-  RouterOutlet,
-  ServiceWorkerRegister,
+	QwikCityProvider,
+	RouterOutlet,
+	ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
 
 import "./global.css";
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
+	/**
+	 * The root of a QwikCity site always start with the <QwikCityProvider> component,
+	 * immediately followed by the document's <head> and <body>.
+	 *
+	 * Don't remove the `<head>` and `<body>` elements.
+	 */
 
-  return (
-    <QwikCityProvider>
-      <head>
-        <meta charSet="utf-8" />
-        <link rel="manifest" href="/manifest.json" />
-        <RouterHead />
-        <ServiceWorkerRegister />
-      </head>
-      <body lang="en">
-        <RouterOutlet />
-      </body>
-    </QwikCityProvider>
-  );
+	return (
+		<QwikCityProvider>
+			<head>
+				{/* ANALYTICS */}
+				<script
+					async
+					src="https://phigara.vercel.app/script.js"
+					data-website-id="2ed1b2ce-bf78-43b8-a975-67b742b1a6df"
+				></script>
+
+				<meta charSet="utf-8" />
+				<link rel="manifest" href="/manifest.json" />
+				<RouterHead />
+				<ServiceWorkerRegister />
+			</head>
+			<body lang="en">
+				<RouterOutlet />
+			</body>
+		</QwikCityProvider>
+	);
 });
